a7b76a672d08fe6ff39f0ab326f849d6417d4084
SoLaMente Dev <testercod3@gmail.com>
2023-10-09 11:18:15 -0300

    cors

--- api/server.js
+++ api/server.js
@@ -3,8 +3,18 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const cors = require('cors'); 
+
+const corsOptions = {
+    origin: '*', // ou defina os domínios permitidos separados por vírgula
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+};
 
 server.use(middlewares)
+server.use(cors(corsOptions));
+
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
